6652ae8cc4170fa5540347d62c758735253efcbe
bartlettpsj <paul@infoqss.com>
2022-09-03 22:50:32 +0000

    Update App.tsx and Paul.tsx

--- src/Paul.tsx
+++ src/Paul.tsx
@@ -1,3 +1,14 @@
 export default ({name='Paul Bartlett'}) => <div>Hello {name}</div>
 export const Porky = ({name='Porky Pies'}) => <div>Greetings {name}</div>
+
+export const Something = ({number: number = 786}) => {
+    let a = 123;
+    debugger;
+    
+    return (
+        <div>
+            Output: {a+number}
+        </div>
+    )
+}
  
