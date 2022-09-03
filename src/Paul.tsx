f174426c0a8b7b372aecdc33afc59ed383abac32
bartlettpsj <paul@infoqss.com>
2022-09-03 20:58:47 +0000

    Update App.tsx and Paul.tsx

--- src/Paul.tsx
+++ src/Paul.tsx
@@ -1,3 +1,3 @@
-export default ({name="Paul"}) => <div>Hello {name}</div>
-export const Porky = () => <div>Porky</div>
+export default ({name='Paul Bartlett'}) => <div>Hello {name}</div>
+export const Porky = ({name='Porky Pies'}) => <div>Hi {name}</div>
  
